9e106008c1b775e65d531db94d8c7dc27b220162
mafalatexb <36725928+mafalatexb@users.noreply.github.com>
2018-03-31 17:44:52 -0400

    changed copyright year

--- src/clientversion.h
+++ src/clientversion.h
@@ -23,7 +23,7 @@
 #define DO_STRINGIZE(X) #X
 
 //! Copyright string used in Windows .rc files
-#define COPYRIGHT_STR "2009-" STRINGIZE(COPYRIGHT_YEAR) " " COPYRIGHT_HOLDERS_FINAL
+#define COPYRIGHT_STR "" STRINGIZE(COPYRIGHT_YEAR) " " COPYRIGHT_HOLDERS_FINAL
 
 /**
  * pigeond-res.rc includes this file, but it cannot cope with real c++ code.
