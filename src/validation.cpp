90e2dcbe60aa413b4efba2ad4518bcd003a3dca0
tri <tri282@gmail.com>
2018-09-28 12:04:42 -0700

    new future time allowable window for a block to reject from block 200k

--- src/validation.cpp
+++ src/validation.cpp
@@ -2963,9 +2963,9 @@ static bool ContextualCheckBlockHeader(const CBlockHeader& block, CValidationSta
     // Check timestamp against prev
     if (block.GetBlockTime() <= pindexPrev->GetMedianTimePast())
         return state.Invalid(false, REJECT_INVALID, "time-too-old", "block's timestamp is too early");
-
+    int64_t futureTimeWindow = nHeight > params.getNewFutureWindowBlock() ? MAX_FUTURE_LWMA_TIME : MAX_FUTURE_BLOCK_TIME;
     // Check timestamp
-    if (block.GetBlockTime() > nAdjustedTime + MAX_FUTURE_BLOCK_TIME)
+    if (block.GetBlockTime() > nAdjustedTime + futureTimeWindow)
         return state.Invalid(false, REJECT_INVALID, "time-too-new", "block timestamp too far in the future");
 
     // Reject outdated version blocks when 95% (75% on testnet) of the network has upgraded:
