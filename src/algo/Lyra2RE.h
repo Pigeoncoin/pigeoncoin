12ccd94207d2390ced90a6d5e7e72a6622f9a9e0
farsider350 <farsider350@github.com>
2021-04-18 16:52:56 +1000

    Add algo x21s

---
+++ src/algo/Lyra2RE.h
@@ -0,0 +1,20 @@
+// Copyright (c)  The Bitcoin Core developers
+// Copyright (c) 2017 The Raven Core developers
+// Copyright (c) 2018 The Rito Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+#ifndef LYRA2RE_H
+#define LYRA2RE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void lyra2re_hash(const char* input, char* output);
+void lyra2re2_hash(const char* input, char* output);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
